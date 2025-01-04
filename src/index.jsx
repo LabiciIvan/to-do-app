0b4f7a69207d60dd17bb82945b6e5cb9d6d32afb
Labici Ivan <labici.ioan@yaoo.com>
2025-01-04 23:18:53 +0200

    NA-2: App title rename after repository.

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-import dummyData from './dummy-data.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
