ae3b2408c80717e6a02f7794d8f3ba9969d8c342
Labici Ivan <labici.ioan@yaoo.com>
2025-01-10 00:28:14 +0200

    NA-2: Category and Comment components update.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,49 @@
-export default function Comment() {
+import { useState } from "react";
 
+export default function Comment({comment, onSetDeleteCommentFromTicket, profile = null, onSetHandleCommentReply}) {
+
+  const [reply, setReply] = useState(false);
+  const [content, setContent] = useState('');
+
+  const handleActionOnInput = (action = null) => {
+    setReply(prev => !prev);
+
+    if (action === 'post') {
+      const event = new Date();
+      const replyTimestamp = event.toLocaleString();
+      // Handle post reply
+      const commentReply = {
+        id: null,
+        content,
+        timestamp: replyTimestamp,
+        postedBy: profile
+      }
+      onSetHandleCommentReply(commentReply, comment.id);
+      return;
+    }
+
+    setContent(() => '');
+  }
   return (
     <div>
-      Comment component
+      <p>{comment.timestamp}</p>
+      <p>{comment.content}</p>
+      <div className='additionals'>
+        <div className='actions'>
+          { profile !== null && <small onClick={() => setReply(prev => !prev)}>reply</small>}
+        </div>
+        <div className='abilities'>
+          {
+            (reply && profile !== null) && 
+            <div className='reply'>
+              <input type="text" value={content} onChange={(e) => setContent(() => e.target.value)}/>
+              <button onClick={() => handleActionOnInput('post')}>Post</button>
+              <button onClick={() => handleActionOnInput()}>Cancel</button>
+            </div>
+          }
+        </div>
+      </div>
+      {(comment.owner?.name === profile?.name || comment.owner === null) && <button onClick={() => onSetDeleteCommentFromTicket(comment.id)}>Delete comment</button>}
     </div>
   )
 }
