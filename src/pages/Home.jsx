7ed79a5b8663e163440679cfc9345430d1c04cf7
Labici Ivan <labici.ioan@yaoo.com>
2025-01-31 00:16:11 +0200

    NA-2: Home component page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
+import '../scss/home.scss';
+import logo from '../assets/landing-page-logo.png';
+
 export default function Home() {
+
+  const text = {
+    title: {
+      one: 'To',
+      two: 'Do',
+      hyphen: '-',
+    },
+    welcome: 'Welcome to',
+    subtitleOne: 'The smarter, simpler way to organize',
+    subtitleTwo: 'your ideas and tasks',
+    logoLanding:logo
+  }
+
+  const {welcome, title: {one, two, hyphen}, subtitleOne, subtitleTwo, logoLanding} = text;
+
   return (
     <div className='home'>
-      Home Page
+
+      <div className='hero-section'>
+        <h1 className='welcome'>{welcome}</h1>
+
+        <div className='title'>
+          <h1 className='one'>{one}</h1>
+          <h1 className='hyphen'>{hyphen}</h1>
+          <h1 className='two'>{two}</h1>
+        </div>
+
+        <h1 className='subtitle-one'>{subtitleOne}</h1>
+
+        <h1 className='subtitle-two'>{subtitleTwo}</h1>
+
+        <img className='logo' src={logoLanding} alt="Landing Page Logo" /> 
+      </div>
     </div>
   )
 }
