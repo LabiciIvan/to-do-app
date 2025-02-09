00e66aa67af711b9a4596992d3015fbabe88477f
Labici Ivan <labici.ioan@yaoo.com>
2025-02-09 18:03:59 +0200

    NA-2: InboxReader component for further dev.

--- src/components/inbox/InboxReader.jsx
+++ src/components/inbox/InboxReader.jsx
@@ -1,6 +1,11 @@
-export default function InboxReader({selected, onSetSelected, onSetCloseInboxReaderComponent, item}) {
-  console.log('item', item);
+import { useRef } from 'react';
+import InboxControls from './InboxControls';
 
+export default function InboxReader({selected, currentSelectedParent, onSetSelected, onSetCloseInboxReaderComponent, item, enableEdit = false, onSaveMessage}) {
+  console.log('selected', selected);
+  console.log('current selected', currentSelectedParent);
+
+  const draftReference = useRef('');
 
   const formatTimeStamp = (timestamp) => {
     const date = new Date(timestamp);
@@ -28,27 +33,46 @@ export default function InboxReader({selected, onSetSelected, onSetCloseInboxRea
 
     const messageSlitted = message.split("\n");
 
-    return messageSlitted.map(line =>
-      <>
-      <span>{line}</span>
-      <br />
-      </>
+    return messageSlitted.map((line, index) =>
+      <span key={index}>{line}</span>
     );
   }
 
+  const enableTextForEdit = (enable) => {
+    if (enable && enableEdit) {
+      console.log('enabled editing');
+    }
+  }
+
+  const handleEditOrMessage = (value) => {
+    draftReference.current = value;
+  }
+
+  const beforeClosingTheView = () => {
+    onSetCloseInboxReaderComponent(currentSelectedParent);
+    console.log('close inboxReader');
+    if (currentSelectedParent === 'drafts') {
+      console.log('enters drafts');
+      const itemMessage = {...item, message: draftReference.current}
+      onSaveMessage(itemMessage);
+    }
+  }
+
+
   return (
     <div className={`inbox-reader ${selected && 'selected'}`}>
+      <InboxControls onSetBeforeClosingTheView={beforeClosingTheView}/>
       { (item !== undefined ) &&
       <>
         <div className='reader-header'>
-          <div className='controls'>
-            <i className='bi bi-arrow-left' onClick={() => onSetCloseInboxReaderComponent()}/>
+          {/* <div className='controls'>
+            <i className='bi bi-arrow-left' onClick={() => beforeClosingTheView()}/>
             <i className='bi bi-trash' />
             <i className='bi bi-archive' />
             <i className='bi bi-exclamation-octagon' />
             <i className='bi bi-folder-symlink' />
             <i className='bi bi-envelope-open' />
-          </div>
+          </div> */}
 
           <div className='title-info'>
             <h4>{item.title}</h4>
@@ -70,8 +94,13 @@ export default function InboxReader({selected, onSetSelected, onSetCloseInboxRea
         </div>
 
       <div className='reader-body'>
-        <div className='message-content'>
-          {formatMessage(item.message)}
+        <div className='message-content'
+          onClick={() => enableTextForEdit(true)}
+          contentEditable={enableEdit}
+          onInput={(e) => handleEditOrMessage(e.target.innerText)}
+          suppressContentEditableWarning={true}
+        >
+          {selected !== 'compose' ? formatMessage(item.message) : 'asd'}
         </div>
       </div>
     </>
